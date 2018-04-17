19760bee67c0a084b2523c7f9b83dfa430ac2dba
Jacquelyn M Santos <js92852@srcs.loc>
2018-04-17 11:32:51 -0700

    started working on age and HR

---
+++ HealthApp.js
@@ -0,0 +1,5 @@
+var button = document.getElementById('mHRandRHR');
+button.addEventListener('click', pulseforMin);
+
+function pulseforMin() {
+    var inputHR = document.getElementById('inputPulse').value;

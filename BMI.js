7419fd5ab12ff4f6e8c1e0900aa13eb624e5e404
Rocsana E De Leon-Cifuentes <rd49373@srcs.loc>
2018-04-17 12:10:51 -0700

    Adding code

---
+++ BMI.js
@@ -0,0 +1,8 @@
+var button = document.getElementById('calculateBMI');
+button.addEventListener('click', BMI);
+
+function BMI(){
+	var w = document.getElementById('inputW').value;
+	var h = document.getElementById('inputH').value;
+	var bmi =
+}

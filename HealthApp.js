c099a7893a79e7fcee377a86c7409f3e73142aba
MichelleSantos22 <31743215+MichelleSantos22@users.noreply.github.com>
2018-04-23 07:28:05 -0700

    worked on rHR and mHR

--- HealthApp.js
+++ HealthApp.js
@@ -4,14 +4,20 @@ var age = document.getElementById('inputAge').value;
 var pulse = document.getElementById('inputPulse').value;
 
 var rHR = resting(pulse);
-
 function everything() {
-    document.getElementById("results").textContent = "Your resting heart rate is " + present + "."
+    document.getElementById("results").textContent = "Your resting heart rate is " + present + ". Your Maximum Heart Rate is " + later + ". "
 }
-
 function resting(rHR) {
     present = (rHR * 10);
     return present;
 }
+
+var mRH = max(age);
+function max(mRH) {
+    later = (220 - mRH);
+    return later
+}
+
+
 
 button.addEventListener('click', everything);

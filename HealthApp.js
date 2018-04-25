e05c127d277b9bd8f0b1b89288d06e19284ba887
Jacquelyn M Santos <js92852@srcs.loc>
2018-04-25 12:25:09 -0700

    i fixed it.

--- HealthApp.js
+++ HealthApp.js
@@ -1,11 +1,11 @@
 var button = document.getElementById('zones');
 
 function everything() {
-    var rHR = resting(pulse);
-    var mHR = maximum(age);
-
     var age = document.getElementById('inputAge').value;
     var pulse = document.getElementById('inputPulse').value;
+
+    var rHR = resting(pulse);
+    var mHR = maximum(age);
 
     document.getElementById("results").textContent = "Your resting heart rate is " + present + " beats per minute. Your Maximum Heart Rate is " + later + "."
 }

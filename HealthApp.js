11286bd8b13548b24a6ae6d1127914f5a93f47d3
MichelleSantos22 <31743215+MichelleSantos22@users.noreply.github.com>
2018-04-24 18:36:55 -0700

    moved the var rHR and var mHR

--- HealthApp.js
+++ HealthApp.js
@@ -3,21 +3,21 @@ var button = document.getElementById('mHRandRHR');
 var age = document.getElementById('inputAge').value;
 var pulse = document.getElementById('inputPulse').value;
 
-var rHR = resting(pulse);
+
 function everything() {
-    document.getElementById("results").textContent = "Your resting heart rate is " + present + ". Your Maximum Heart Rate is " + later + ". "
+    var rHR = resting(pulse);
+    var mRH = maximum(age);
+    document.getElementById("results").textContent = "Your resting heart rate is " + present + ". Your Maximum Heart Rate is " + later + "."
 }
+
 function resting(rHR) {
     present = (rHR * 10);
     return present;
 }
 
-var mRH = max(age);
-function max(mRH) {
+function maximum(mRH) {
     later = (220 - mRH);
-    return later
+    return later;
 }
-
-
 
 button.addEventListener('click', everything);

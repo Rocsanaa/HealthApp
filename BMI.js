9b85155cdb7469d7fc33085a22d52d7e5f8f7ab8
MichelleSantos22 <31743215+MichelleSantos22@users.noreply.github.com>
2018-04-27 07:39:44 -0700

    today 4/27

--- BMI.js
+++ BMI.js
@@ -1,21 +1,23 @@
-var button = document.getElementById('bmi');
+var button2 = document2.getElementById('bmi');
 
 function final() {
+    var feet = document2.getElementById('inputft').value;
+    var pounds = document2.getElementById('inputW').value;
 
-    var height = document.getElementById('inputft').value;
-    var weight = document.getElementById('inputW').value;
+    var first = poundstokg(pounds);
+    var second = fttometers(feet);
 
-    document.getElementById("product").textContent = "Your BMI score is " + present + ". Your BMI category is " + later + "."
+    document2.getElementById("product").textContent = "Your BMI score is " +  + ". Your BMI category is " +  + "."
 }
 
-function fttometers() {
- meters = height / 0.3048;
+function poundstokg(first) {
+    kg = (pounds * 0.453592);
+    return kg;
+}
+
+function fttometers(second) {
+    meters = (feet * 0.3048);
     return meters;
 }
 
-function poundstokg() {
-
-}
-
-
-button.addEventListener('click', final);
+button2.addEventListener('click', final);

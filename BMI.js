d7b5ee1cf2979e619c6f5b28d5339ba58eab91a0
Jacquelyn M Santos <js92852@srcs.loc>
2018-04-27 11:43:03 -0700

    h

--- BMI.js
+++ BMI.js
@@ -1,22 +1,23 @@
 var button2 = document.getElementById('bmi');
 
-    var feet = document.getElementById('inputft').value;
-    var pounds = document.getElementById('inputW').value;
+
 
 function final() {
-    var first = poundstokg(pounds);
-    var second = fttometers(feet);
+    var feet = document.getElementById('inputft').value;
+    var pounds = document.getElementById('inputW').value;
+    var kilos = poundstokg(pounds);
+    var m = fttometers(feet);
 
-    document.getElementById("product").textContent = "Your weight in kg is " + kg + ". Your height in meters is " + meters + "."
+    document.getElementById("product").textContent = "Your weight in kg is " + kilos + ". Your height in meters is " + m + "."
 }
 
-function poundstokg(first) {
-    kg = (pounds * 0.453592);
+function poundstokg(pounds) {
+    var kg = (pounds * 0.453592);
     return kg;
 }
 
-function fttometers(second) {
-    meters = (feet * 0.3048);
+function fttometers(feet) {
+    var meters = (feet * 0.3048);
     return meters;
 }
 

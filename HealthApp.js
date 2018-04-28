cd3e7803338549390cb89f45f2fc773d0b6e5a5c
MichelleSantos22 <31743215+MichelleSantos22@users.noreply.github.com>
2018-04-27 17:55:48 -0700

    done with training zones too!

--- HealthApp.js
+++ HealthApp.js
@@ -21,3 +21,24 @@ function maximum(mHR) {
 }
 
 button.addEventListener('click', everything);
+
+
+var button3 = document.getElementById('levels');
+
+function phase() {
+    var zone1a = later * 0.5;
+    var zone1b = later * 0.6;
+    var zone2a = later * 0.6;
+    var zone2b = later * 0.7;
+    var zone3a = later * 0.7;
+    var zone3b = later * 0.8;
+    var zone4a = later * 0.8;
+    var zone4b = later * 0.9;
+    var zone5a = later * 0.9;
+    var zone5b = later * 1.0;
+
+    document.getElementById("triple").textContent = "Zone 1: " + zone1a + " - " + zone1b + " BPM. Zone 2: " + zone2a + " - " + zone2b + " BPM. Zone 3: " + zone3a + " - " + zone3b + " BPM. Zone 4: " + zone4a + " - " + zone4b + " BPM. Zone 5: " + zone5a + " - " + zone5b + " BPM."
+
+}
+
+button3.addEventListener('click', phase);

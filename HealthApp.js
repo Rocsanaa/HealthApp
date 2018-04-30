7140d6b5826e345ad1ab36bcd13b2c1f8531fa7d
MichelleSantos22 <31743215+MichelleSantos22@users.noreply.github.com>
2018-04-29 20:26:42 -0700

    added math.floor

--- HealthApp.js
+++ HealthApp.js
@@ -26,16 +26,16 @@ button.addEventListener('click', everything);
 var button3 = document.getElementById('levels');
 
 function phase() {
-    var zone1a = later * 0.5;
-    var zone1b = later * 0.6;
-    var zone2a = later * 0.6;
-    var zone2b = later * 0.7;
-    var zone3a = later * 0.7;
-    var zone3b = later * 0.8;
-    var zone4a = later * 0.8;
-    var zone4b = later * 0.9;
-    var zone5a = later * 0.9;
-    var zone5b = later * 1.0;
+    var zone1a = Math.floor(later * 0.5);
+    var zone1b = Math.floor(later * 0.6);
+    var zone2a = Math.floor(later * 0.6);
+    var zone2b = Math.floor(later * 0.7);
+    var zone3a = Math.floor(later * 0.7);
+    var zone3b = Math.floor(later * 0.8);
+    var zone4a = Math.floor(later * 0.8);
+    var zone4b = Math.floor(later * 0.9);
+    var zone5a = Math.floor(later * 0.9);
+    var zone5b = Math.floor(later * 1.0);
 
     document.getElementById("triple").textContent = "Zone 1: " + zone1a + " - " + zone1b + " BPM. Zone 2: " + zone2a + " - " + zone2b + " BPM. Zone 3: " + zone3a + " - " + zone3b + " BPM. Zone 4: " + zone4a + " - " + zone4b + " BPM. Zone 5: " + zone5a + " - " + zone5b + " BPM."
 
